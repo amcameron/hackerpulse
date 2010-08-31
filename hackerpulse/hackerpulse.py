0ce108e84b771e4f252ab38609c057d566e84043
Yrb <bryashman@gmail.com>
2010-08-31 20:54:43 +1200

    Just mocking things out a little

--- hackerpulse/hackerpulse.py
+++ hackerpulse/hackerpulse.py
@@ -7,7 +7,21 @@ def index():
 
 @app.route('/<username>')
 def user_pulse(username):
-    return render_template('user_pulse.html')
+    context = {
+        'username': username,
+        'bio': """Edit your bio here, enter some information about you. This is
+ customizable and you can add a quote or something here, maybe a link to your
+personal site or where you work. It's really up to you!""",
+
+        'pulse_feed': [
+            dict(source='github', text='Commited changes to <a>swanson/mongo-overflow</a> repository.'),
+            dict(source='hacker-news', text='Posted a comment on <a>Ask HN: What projects are you working on?</a>'),
+            dict(source='reddit', text='Posted a comment on <a>Google App Utilities for Python</a>'),
+            dict(source='irl', text='Attended <a>2010 SocialDevCamp Chicago Hackathon</a>'),
+            ]
+        }
+    
+    return render_template('user_pulse.html', **context)
 
 if '__main__' == __name__:
     app.run()
